ff2e68718ae1370e17f595362243b1ed7a6a3cc5
weiyolan <yolan.weiler@gmail.com>
2022-08-26 13:05:46 +0200

    logo color randomization

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,14 @@
-function random() {
-    return Math.random()
+let logo = document.getElementById('headerLogo');
+
+function randomDegree() {
+    return Math.floor(Math.random()*361);
 }
 
-console.log("This is a test run.");
-console.log(Math.random());
+function setRandomColor() {
+    logo.style.filter = 'invert(57%) sepia(52%) saturate(4679%) hue-rotate(' + randomDegree() + 'deg) brightness(93%) contrast(101%)';
+    setTimeout(setRandomColor(),500);
+}
+
+logo.addEventListener('load',setRandomColor());
+
+// setInterval(logo.addEventListener('load',setRandomColor),500);
